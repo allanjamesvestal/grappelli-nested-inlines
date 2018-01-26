0c38681a7acffb286d363f71fda2618186e949ca
Allan James Vestal <allanjamesvestal@gmail.com>
2018-01-25 18:07:16 -0600

    Update ownership information. Roll v0.6.1, first release from @allanjamesvestal.

--- setup.py
+++ setup.py
@@ -1,25 +1,35 @@
-#!/usr/bin/python
-from setuptools import setup, find_packages
+# Imports from python.  # NOQA
+import os
+from setuptools import find_packages
+from setuptools import setup
 
 
+# Imports from grappelli-nested.
 from grappelli_nested import __version__
 
 
-github_url = 'https://github.com/datahub/grappelli-nested-inlines'
-github_tag_version = '0.6.0'
+REPO_URL = 'https://github.com/allanjamesvestal/grappelli-nested-inlines/'
+
+PYPI_VERSION = '.'.join(str(v) for v in __version__)
+
+DESCRIPTION = 'Enables nested inlines in the Django/Grappelli admin'
 
 
 setup(
     name='grappelli-nested-inlines',
-    version='.'.join(str(v) for v in __version__),
-    description='Enables nested inlines in the Django/Grappelli admin',
-    url=github_url,
-    download_url='%s/tarball/%s' % (github_url, github_tag_version),
-    author='Allan James Vestal (based on code by Alain Trinh)',
-    author_email='datahub@jrn.com',
+    version=PYPI_VERSION,
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     license='MIT License',
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    url=REPO_URL,
+    download_url='{repo_url}archive/{version}.tar.gz'.format(**{
+        'repo_url': REPO_URL,
+        'version': PYPI_VERSION,
+    }),
+    author='Allan James Vestal (based on code by Alain Trinh)',
+    author_email='allanjamesvestal@gmail.com',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
